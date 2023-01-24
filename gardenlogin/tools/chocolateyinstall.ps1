c5a29b738470f697436152b922d6757dcfdd8ff9
gardener-robot-ci-1 <gardener.ci.user@gmail.com>
2023-01-24 19:44:24 +0000

    Update gardenlogin

--- gardenlogin/tools/chocolateyinstall.ps1
+++ gardenlogin/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 $ErrorActionPreference = 'Stop'
-$url64      = "https://github.com/gardener/gardenlogin/releases/download/v0.3.0/gardenlogin_windows_amd64.exe"
+$url64      = "https://github.com/gardener/gardenlogin/releases/download/v3.0.14/gardenlogin_windows_amd64.exe"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   PackageName     = $env:ChocolateyPackageName
   Url64bit        = $url64
   ChecksumType64  = 'sha256'
-  Checksum64      = "caff78d41f64d7a3383c0538612d2bd616e679bb37a8691bc55138158511f251"
+  Checksum64      = "3d43aac1063c47c745d84f30e5baa519ef92fb8fb1462e5caa6a3ffc5850aedb"
   FileFullPath    = "$toolsDir\gardenlogin.exe"
 }
 Get-ChocolateyWebFile @packageArgs
